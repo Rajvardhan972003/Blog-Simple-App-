8057035c010d06e87e456bd94b87bd573f10fc98
Rajvardhan972003 <157830171+Rajvardhan972003@users.noreply.github.com>
2025-05-16 18:27:46 +0530

    Create data.py

---
+++ BloggingApp-Flask-main/data.py
@@ -0,0 +1,17 @@
+def Articles():
+    return [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'This is article one',
+            'author': 'Author One',
+            'create_date': '2025-01-01'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'This is article two',
+            'author': 'Author Two',
+            'create_date': '2025-01-02'
+        }
+    ]
